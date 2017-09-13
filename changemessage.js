bb125c9e6b3bd1dd51a33cc8c8000667ebe78079
Chewsterchew <chew@chew.pw>
2017-09-13 16:26:04 -0500

    remove old assets

--- changemessage.js
+++
@@ -1,6 +0,0 @@
-function changeMessage() {
-  for (var i = 0; i < 4; i++) {
-    document.getElementById(i + "line").innerHTML = lines2[i][(Math.floor(Math.random() * lines2[i].length))];
-  }
-  document.getElementById("startBttn").textContent = "Again?";
-}
