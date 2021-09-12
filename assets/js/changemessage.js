1f30d828d6507593e1921ace9c234fc8613fa0e5
Chew <chew@chew.pw>
2021-09-12 17:21:48 -0500

    Move words to JSON, rewrite JS a bit, and fix some errors

--- assets/js/changemessage.js
+++ assets/js/changemessage.js
@@ -1,6 +1,19 @@
+/**
+ * Changes what's displayed on the main page.
+ * That really _blank_ my _blank_.
+ */
 function changeMessage() {
-  for (var i = 0; i < 4; i++) {
-    document.getElementById(i + "line").innerHTML = lines2[i][(Math.floor(Math.random() * lines2[i].length))];
-  }
-  document.getElementById("startBttn").textContent = "Again?";
+    for (let i = 0; i < 4; i++) {
+        document.getElementById(i + "line").innerHTML = getRandomWord(lines2[i]);
+    }
+    document.getElementById("startBttn").textContent = "Again?";
+}
+
+/**
+ * Gets a random word from the array
+ * @param {String[]} array The array to get a word from
+ * @returns {String} A word
+ */
+function getRandomWord(array) {
+    return array[Math.floor(Math.random()*array.length)];
 }
